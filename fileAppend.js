af0bb391f986ef68d569cb9fb815f62aa3aa27e5
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-05 10:19:20 +0530

    Added post functionality for express

---
+++ fileAppend.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+function fileAppend(filePath,data) {
+  fs.appendFile(filePath, data, (err) => {
+    if (err) throw err;
+    console.log('The data was appended to file!');
+  });
+}
+module.exports =fileAppend;
--- serverExpressGetPost.js
+++ serverExpressGetPost.js
@@ -1,13 +1,23 @@
 const port = 3000;
+const filePath = '/Users/aritraaritra/Documents/nodeExercises/sampleTextFile.txt';
 const express = require('express')
-const app = express()
-var readFile = require('./syncFileRead');
-app.get('/read', (request, response)=> {
- const filePath = '/Users/aritraaritra/Documents/nodeExercises/sampleTextFile.txt';
- const result = readFile(filePath);
- response.send(result);
-});
+const bodyParser = require('body-parser');
+const readFile = require('./syncFileRead');
+const appendFile = require('./fileAppend');
+const app = express();
 
-app.listen(port,()=>{
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.get('/read', (request, response) => {
+  const result = readFile(filePath);
+  response.send(result);
+});
+app.post('/write', (req, res) => {
+  const data = req.body.dataWrite;
+  appendFile(filePath, `${data}\n`);
+});
+app.listen(port, () => {
   console.log('Listening');
 });
