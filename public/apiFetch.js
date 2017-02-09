386957ba43499aa7a6d74d6623b74a3014673e7d
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-09 18:12:37 +0530

    Something works

--- public/apiFetch.js
+++ public/apiFetch.js
@@ -8,7 +8,22 @@ let entityMap = {
   '`': '&#x60;',
   '=': '&#x3D;'
 };
-
+function addToHtml(task) {
+  let checked = task.status == true ? 'checked' : null
+  let string = `<tr id='${task.id}-desc-status'>
+        <td>
+        <input class="desc-design ${checked ? 'striked' : ''}" type='text' id='${task.id}-desc' value='${task.description}' onfocusout='updateList(${task.id})'></input>
+        </td>
+        <td>
+        <input type="checkbox" id="${task.id}-chk" value="${task.status}" onchange="updateStatus(${task.id})" ${checked}>
+        </td>
+        <td>
+        <button id="${task.id}" type="button" onclick="deleteTask
+        (${task.id})">❌</button>
+        </td>
+        </tr>`
+  return string
+}
 function escapeHtml(string) {
   return String(string).replace(/[&<>"'`=\/]/g, function (s) {
     return entityMap[s];
@@ -32,19 +47,10 @@ function outputList() {
         let id = escapeHtml(index + 1)
         let desc = escapeHtml(task.description)
         let status = escapeHtml(task.status)
-        let checked = task.status == true ? 'checked' : '\0'
-        listElement.innerHTML += `<tr id='${task.id}-desc-status'>
-        <td>
-        <input class="desc-design" type='text' id='${task.id}-desc' value='${desc}' onfocusout='updateList(${task.id})'></input>
-        </td>
-        <td>
-        <input type="checkbox" id="${task.id}-chk" value="${task.status}" onchange="updateStatus(${task.id})" ${checked}>
-        </td>
-        <td>
-        <button id="${task.id}" type="button" onclick="deleteTask
-        (this.id)">❌</button>
-        </td>
-        </tr>`
+        let checked = task.status == true ? 'checked' : null
+        console.log(checked)
+        listElement.innerHTML += addToHtml(task)
+
       })
     })
     .catch(function (err) {
@@ -68,27 +74,32 @@ function writeList() {
   description = document.getElementById('data').value = ''
 }
 function deleteTask(id) {
-  console.log(id)
+  const listElement = document.getElementById(`${id}-desc-status`)
+  let description = document.getElementById(`${id}-desc`).value
+  let statusBool = document.getElementById(`${id}-chk`).value
+   let taskObj = {
+    id, description, statusBool
+  }
   fetch(`/destroy/${id}`, { method: 'delete' })
-    .then((response) => {
-      console.log(response)
-      return outputList()
+    .then(() => {
+      listElement.parentNode.removeChild(listElement)
     })
-    .then()
+    
     .catch(function (err) {
       console.log(err)
     })
 }
 function updateList(id) {
-  //let id = document.getElementById('lineNumber').value
   const listElement = document.getElementById(`${id}-desc-status`)
   let description = document.getElementById(`${id}-desc`).value
   let statusBool = document.getElementById(`${id}-chk`).value
-  let checked = status == true ? 'checked' : '\0'
+  let checked = status == true ? 'checked' : null
+  let taskObj = {
+    id, description, statusBool
+  }
   let data = {
     description: description,
   }
-  //console.log(description)
   fetch(`/update/${id}`, {
     method: 'put',
     body: JSON.stringify(data),
@@ -96,24 +107,8 @@ function updateList(id) {
       "Content-type": "application/json"
     }
   })
-    .then((res) => {
-      return res.json()
-      //return outputList()
-    })
-    .then((text) => {
-      const idTask = (text[0][0].id)
-      listElement.innerHTML = `<tr id='${idTask}-desc-status'>
-        <td>
-        <input class="desc-design" type='text' id='${idTask}-desc' value='${description}' onfocusout='updateList(${idTask})'></input>
-        </td>
-        <td>
-        <input type="checkbox" id="${idTask}-chk" value="${statusBool}" onchange="updateStatus(${id})" ${checked}>
-        </td>
-        <td>
-        <button id="${idTask}" type="button" onclick="deleteTask
-        (${idTask})">❌</button>
-        </td>
-        </tr>`
+    .then(() => {
+      listElement.innerHTML = addToHtml(taskObj)
     })
     .catch(function (err) {
       console.log(err)
@@ -123,14 +118,14 @@ function updateStatus(id) {
   const listElement = document.getElementById(`${id}-desc-status`)
   let description = document.getElementById(`${id}-desc`).value
   let status = document.getElementById(`${id}-chk`).value
-  console.log(status)
   status = (status == 'false') ? true : false
-  let checked = status == true ? 'checked' : '\0'
-  
+  let checked = status == true ? 'checked' : null
+  let taskObj = {
+    id, description, status
+  }
   let data = {
     status: status
   }
-
   fetch(`/update/${id}`, {
     method: 'put',
     body: JSON.stringify(data),
@@ -138,23 +133,8 @@ function updateStatus(id) {
       "Content-type": "application/json"
     }
   })
-    .then((res) => {
-      return res.json()
-    })
-    .then((text) => {
-      const idTask = (text[0][0].id)
-      listElement.innerHTML = `<tr id='${idTask}-desc-status'>
-        <td>
-        <input class="desc-design" type='text' id='${idTask}-desc' value='${description}' onfocusout='updateList(${idTask})'></input>
-        </td>
-        <td>
-        <input type="checkbox" id="${idTask}-chk" value="${status}" onchange="updateStatus(${id})" ${checked}>
-        </td>
-        <td>
-        <button id="${idTask}" type="button" onclick="deleteTask
-        (${idTask})">❌</button>
-        </td>
-        </tr>`
+    .then(() => {
+      listElement.innerHTML = addToHtml(taskObj)
     })
     .catch(function (err) {
       console.log(err)
