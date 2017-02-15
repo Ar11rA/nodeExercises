d4fe4309bf4a27fa03f8aa0cb12ce2296201aba9
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-15 14:35:11 +0530

    Added few more tests

--- fileAppend.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-function fileAppend(filePath,data) {
-  fs.appendFile(filePath, `${data}\n`, (err) => {
-    if (err) throw err
-    console.log('The data was appended to file!')
-  })
-}
-module.exports =fileAppend
